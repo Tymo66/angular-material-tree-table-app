bebd51fa6d5fc97e7339645b18584f6b965243a0
Voorthuijsen, Tijmen van <tijmen.vanvoorthuijsen@wur.nl>
2023-07-24 17:28:03 +0200

    fix: added router module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TreeTableComponent } from './tree-table/tree-table.component';
@@ -13,6 +14,7 @@ import { TreeTableComponent } from './tree-table/tree-table.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     MatTreeModule,
     MatTableModule,
     MatSelectModule,
